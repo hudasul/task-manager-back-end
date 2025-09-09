25da8c24de6761538fc6bb10b3fcabfefa246421
Bint Alhuda Sultan <bntalhdysltan@gmail.com>
2025-09-09 11:27:13 +0300

    update user password

--- controllers/auth.js
+++ controllers/auth.js
@@ -41,12 +41,37 @@ exports.login = async (req, res) => {
     }
 
     const payload = {
-
+      username: user.username,
       id: user._id,
     };
     const token = jwt.sign(payload, SECRET, { expiresIn: "1h" });
     res.json({ token });
   } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+exports.updatePassword = async (req, res) => {
+  const { userId, oldPassword, newPassword } = req.body;
+
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const isMatch = await bcrypt.compare(oldPassword, user.passwordHash);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Incorrect old password" });
+    }
+
+    const newHash = await bcrypt.hash(newPassword, 8);
+    user.passwordHash = newHash;
+    await user.save();
+
+    res.status(200).json({ message: "Password updated successfully" });
+  } catch (error) {
+    console.error("Password update error:", error);
     res.status(500).json({ message: "Server error" });
   }
 };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,5 +4,6 @@ const authController = require("../controllers/auth");
 
 router.post("/signup", authController.register);
 router.post("/login", authController.login);
+router.put("/updatePassword", authController.updatePassword)
 
 module.exports = router;
